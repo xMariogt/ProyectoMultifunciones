b2b9b84d20697943305bf386a68114c25b1a9a7c
Mario Chavarria <jorgemarioportillo90@gmail.com>
2022-12-15 07:56:47 -0600

    Se agregó funcion restar y en el principal son utiles las funciones de suma y resta

--- funciones_aritmeticas.py
+++ funciones_aritmeticas.py
@@ -1,2 +1,5 @@
 def sumar(numero1, numero2):
     return numero1+numero2
+
+def restar(numero1, numero2):
+    return numero1-numero2
--- principal.py
+++ principal.py
@@ -1,11 +1,25 @@
 import funciones_aritmeticas
 
-print("Escoja la operacion que desea realizar: ")
-print("1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir\n0.Salir")
-opcion = int(input())
+opcion = 1
 
-if(opcion == 1):
-    numero1 = int(input("Ingrese el primer valor: "))
-    numero2 = int(input("Ingrese el segundo valor: "))
-    resultado = funciones_aritmeticas.sumar(numero1,numero2)
-    print(resultado)
+while(opcion != 0):
+    print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
+    print("Escoja la operacion que desea realizar: ")
+    print("1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir\n0.Salir")
+    opcion = int(input())
+    print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-")
+
+    if(opcion == 1):#Se realizará una suma
+        numero1 = int(input("Ingrese el primer valor: "))
+        numero2 = int(input("Ingrese el segundo valor: "))
+        resultado = funciones_aritmeticas.sumar(numero1,numero2)
+        print(f'El resultado es: {resultado}')
+    elif(opcion == 2):
+        numero1 = int(input("Ingrese el primer valor: "))
+        numero2 = int(input("Ingrese el segundo valor: "))
+        resultado = funciones_aritmeticas.restar(numero1,numero2)
+        print(f'El resultado es: {resultado}')
+
+
+
+
