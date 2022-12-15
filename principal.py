f8defba4d05849061b06f63a4f930bce20660ede
Mario Chavarria <jorgemarioportillo90@gmail.com>
2022-12-15 07:17:05 -0600

    Se agregó menú y la funcion de suma

--- principal.py
+++ principal.py
@@ -1,3 +1,11 @@
-print("Hola Mundo")
-print("Esto es una linea de prueba")
-print("realizamos un nuevo cambio de prueba")
+import funciones_aritmeticas
+
+print("Escoja la operacion que desea realizar: ")
+print("1.Sumar\n2.Restar\n3.Multiplicar\n4.Dividir\n0.Salir")
+opcion = int(input())
+
+if(opcion == 1):
+    numero1 = int(input("Ingrese el primer valor: "))
+    numero2 = int(input("Ingrese el segundo valor: "))
+    resultado = funciones_aritmeticas.sumar(numero1,numero2)
+    print(resultado)
